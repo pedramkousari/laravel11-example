6347a3c7b1bd444aa862e6f014b77b39c92f00e6
pedram kousari <persianped@gmail.com>
2024-12-13 15:55:48 +0330

    laravel-breeze installed

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,10 +2,7 @@
 
 namespace App\Providers;
 
-use Illuminate\Support\Facades\DB;
-use Illuminate\Support\Facades\URL;
 use Illuminate\Support\Facades\Vite;
-use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -23,40 +20,6 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        $this->configureCommands();
-        $this->configureModels();
-        $this->configureUrl();
-        $this->configureVite();
-    }
-
-    private function configureCommands(): void
-    {
-        DB::prohibitDestructiveCommands(
-            app()->environment('production')
-        );
-    }
-
-    private function configureModels(): void
-    {
-        Model::shouldBeStrict();
-        Model::unguard();
-    }
-
-    /**
-     * Configure the application's URL.
-     */
-    private function configureUrl(): void
-    {
-        if (app()->environment('production')) {
-            URL::forceScheme('https');
-        }
-    }
-
-    /**
-     * Configure the application's Vite.
-     */
-    private function configureVite(): void
-    {
-        Vite::usePrefetchStrategy('aggressive');
+        Vite::prefetch(concurrency: 3);
     }
 }
